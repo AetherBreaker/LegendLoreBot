0ab7fd347a926a885c132e277fb08b51ab3a9172
Jacob Ogden <jacob.ogden@yahoo.com>
2025-08-07 23:21:26 -0400

    Saving progress on database cache

--- src/database/cache.py
+++ src/database/cache.py
@@ -6,13 +6,25 @@ if __name__ == "__main__":
 
 from logging import getLogger
 
+from environment_init_vars import GOOGLE_API_KEY_FILE, SETTINGS
+from gspread import service_account
+from gspread.http_client import BackOffHTTPClient
+from pandas import DataFrame
 from typing_custom.abc import SingletonType
 
 logger = getLogger(__name__)
 
+SERVICE_ACCOUNT = service_account(filename=GOOGLE_API_KEY_FILE, http_client=BackOffHTTPClient)
 
 
 class DatabaseCache(metaclass=SingletonType):
-  def __init__(self) -> None: ...
+  _guilds_tab_id = SETTINGS.database_guilds_id
+  _users_tab_id = SETTINGS.database_users_id
+  _characters_tab_id = SETTINGS.database_characters_id
+
+  def __init__(self) -> None:
+    self._guild_cache: DataFrame
+    self._user_cache: DataFrame
+    self._character_cache: DataFrame
 
   async def refresh_cache(self) -> None: ...
--- src/environment_init_vars.py
+++ src/environment_init_vars.py
@@ -1,3 +1,4 @@
+# sourcery skip: raise-from-previous-error
 if __name__ == "__main__":
   from logging_config import configure_logging
 
@@ -16,6 +17,15 @@ SETTINGS = Settings()  # type: ignore
 
 # Folder paths
 CWD = Path.cwd()
+
+try:
+  GOOGLE_API_KEY_FILE = next(CWD.glob("bot-db-key.json"))
+except StopIteration:
+  raise FileNotFoundError(
+    "Google API key file 'bot-db-key.json' not found in the current directory.\n"
+    "Please create a service account key in the Google Cloud Console "
+    "and save it as 'bot-db-key.json' in the current directory."
+  )
 
 
 OWNER_IDS = {200632489998417929}
