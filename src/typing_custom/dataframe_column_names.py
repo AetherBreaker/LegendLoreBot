ada98c2b55ba9698eec27c9db5598855e6ce602c
Jacob Ogden <jacob.ogden@yahoo.com>
2025-08-08 23:06:32 -0400

    Added enumerations for defining the names, order, and other metadata of dataframe columns

--- src/typing_custom/dataframe_column_names.py
+++ src/typing_custom/dataframe_column_names.py
@@ -13,6 +13,7 @@ logger = getLogger(__name__)
 class ColNameEnum(StrEnum):
   __exclude__ = []
   __init_include__ = []
+  __index_items__ = []
 
   @classmethod
   def ordered_column_names(cls, *columns: str) -> list[str]:
@@ -21,11 +22,7 @@ class ColNameEnum(StrEnum):
 
   @classmethod
   def all_columns(cls) -> list[str]:
-    return [
-      str(column)
-      for column in cls
-      if str(column) not in cls.__exclude__ and not str(column).startswith("_")
-    ]
+    return [str(column) for column in cls if str(column) not in cls.__exclude__ and not str(column).startswith("_")]
 
   @classmethod
   def err_reporting_columns(cls) -> list[str]:
@@ -36,22 +33,14 @@ class ColNameEnum(StrEnum):
     return [
       "err_field_name",
       "err_reason",
-      *[
-        str(column)
-        for column in cls
-        if str(column) not in cls.__exclude__ and not str(column).startswith("_")
-      ],
+      *[str(column) for column in cls if str(column) not in cls.__exclude__ and not str(column).startswith("_")],
     ]
 
   @classmethod
   def init_columns(cls) -> list[str]:
     if not cls.__init_include__:
       return cls.all_columns()
-    return [
-      str(column)
-      for column in cls
-      if str(column) in cls.__init_include__ and not str(column).startswith("_")
-    ]
+    return [str(column) for column in cls if str(column) in cls.__init_include__ and not str(column).startswith("_")]
 
   @classmethod
   def testing_columns(cls) -> list[str]:
@@ -67,3 +56,32 @@ class ColNameEnum(StrEnum):
     Return the member name.
     """
     return name
+
+
+class DatabaseGuildsColumns(ColNameEnum):
+  __index_items__ = ["guild_id"]
+  guild_id = "guild_id"
+  guild_name = "guild_name"
+
+
+class DatabaseUsersColumns(ColNameEnum):
+  __index_items__ = ["user_id", "guild_id"]
+  user_id = "user_id"
+  guild_id = "guild_id"
+  user_name = "user_name"
+  server_money = "server_money"
+
+
+class DatabaseCharactersColumns(ColNameEnum):
+  __index_items__ = ["character_uid", "user_id", "guild_id"]
+  character_uid = "character_uid"
+  user_id = "user_id"
+  guild_id = "guild_id"
+  character_name = "character_name"
+  milestones = "milestones"
+  mythic_trials = "mythic_trials"
+  money_pp = "money_pp"
+  money_gp = "money_gp"
+  money_sp = "money_sp"
+  money_cp = "money_cp"
+  downtime_stockpiled = "downtime_stockpiled"
