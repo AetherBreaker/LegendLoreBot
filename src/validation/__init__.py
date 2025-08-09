3f9f92cd7dadd4d4a1149c982449176b5b79185b
Jacob Ogden <jacob.ogden@yahoo.com>
2025-08-08 23:05:21 -0400

    Added some validation wrappers to allow visibility of extra metadata while debugging database validation

--- src/validation/__init__.py
+++ src/validation/__init__.py
@@ -4,8 +4,18 @@ if __name__ == "__main__":
   configure_logging()
 
 from logging import getLogger
+from typing import Any, Self
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import (
+  BaseModel,
+  ConfigDict,
+  ModelWrapValidatorHandler,
+  ValidationError,
+  ValidationInfo,
+  ValidatorFunctionWrapHandler,
+  field_validator,
+  model_validator,
+)
 
 logger = getLogger(__name__)
 
@@ -18,3 +28,30 @@ class CustomBaseModel(BaseModel):
     validate_assignment=True,
     coerce_numbers_to_str=True,
   )
+
+  @field_validator("*", mode="wrap", check_fields=False)
+  @classmethod
+  def log_failed_field_validations(cls, data: str, handler: ValidatorFunctionWrapHandler, info: ValidationInfo) -> Any:
+    results = None
+
+    try:
+      results = handler(data)
+    except ValidationError as e:
+      exc_type, exc_val, exc_tb = type(e), e, e.__traceback__
+
+      raise e
+
+    return data if results is None else results
+
+  @model_validator(mode="wrap")
+  @classmethod
+  def log_failed_validation(cls, data: Any, handler: ModelWrapValidatorHandler[Self], info: ValidationInfo) -> Self:
+    results = None
+    try:
+      results = handler(data)
+    except ValidationError as e:
+      exc_type, exc_val, exc_tb = type(e), e, e.__traceback__
+
+      raise e
+
+    return data if results is None else results
