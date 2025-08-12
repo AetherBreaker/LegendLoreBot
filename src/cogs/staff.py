98838b3b1ab063110f3a3577af240cdb77b0d1f2
Jacob Ogden <jacob.ogden@yahoo.com>
2025-08-11 22:19:46 -0400

    Added the first functioning command for database testing

--- src/cogs/staff.py
+++ src/cogs/staff.py
@@ -6,9 +6,10 @@ if __name__ == "__main__":
 from logging import getLogger
 from typing import TYPE_CHECKING
 
-from bot_base import SwallowBot
-from disnake import ApplicationCommandInteraction
+from disnake import ApplicationCommandInteraction, GuildCommandInteraction
 from disnake.ext.commands import Cog, slash_command
+from pydantic import ValidationError
+from validation.models.database import CharacterDBEntryModel
 
 logger = getLogger(__name__)
 
@@ -23,20 +24,43 @@ class StaffCommands(Cog):
     self._last_member = None
 
   @slash_command()
-  async def staff(self, _: ApplicationCommandInteraction): ...
+  async def staff(self, _: GuildCommandInteraction): ...
 
   @staff.sub_command_group()
-  async def characters(self, _: ApplicationCommandInteraction): ...
+  async def characters(self, _: GuildCommandInteraction): ...
+
+  @characters.sub_command()
+  async def add(self, inter: GuildCommandInteraction, character_name: str, sheetlink: str):
+    try:
+      new_entry = CharacterDBEntryModel(
+        user_id=inter.author.id,
+        guild_id=inter.guild_id,
+        character_name=character_name,
+        sheet_link=sheetlink,  # type: ignore
+      )
+
+    except ValidationError as e:
+      logger.error(f"Failed to add character: {e}")
+      await inter.send(f"Failed to add character.\n {e}")
+      return
+
+    await self.bot.database.characters.append_row(new_entry)
+
+    await inter.send(
+      f"Successfully added character: {new_entry.character_name}\n"
+      f"Sheet Link: {new_entry.sheet_link}\n"
+      f"Character UID: {new_entry.character_uid}\n"
+      f"User ID: {new_entry.user_id}\n"
+      f"Guild ID: {new_entry.guild_id}\n"
+    )
+
+  @staff.sub_command_group()
+  async def coins(self, _: GuildCommandInteraction): ...
 
   ...
 
   @staff.sub_command_group()
-  async def coins(self, _: ApplicationCommandInteraction): ...
-
-  ...
-
-  @staff.sub_command_group()
-  async def servercoins(self, _: ApplicationCommandInteraction): ...
+  async def servercoins(self, _: GuildCommandInteraction): ...
 
   ...
 
