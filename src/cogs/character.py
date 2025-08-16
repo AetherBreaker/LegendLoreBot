16458ec2b83fabc1b0556029ea3236e862b9668f
Jacob Ogden <jacob.ogden@yahoo.com>
2025-08-16 02:18:52 -0400

    First functional implementation of the character status command

--- src/cogs/character.py
+++ src/cogs/character.py
@@ -25,6 +25,8 @@ class CharacterTracking(Cog):
   @slash_command()
   async def characters(self, _: ApplicationCommandInteraction): ...
 
+  # View character data
+
   @characters.sub_command()
   async def status(
     self,
@@ -40,10 +42,41 @@ class CharacterTracking(Cog):
     if guild is None:
       guild = await self.bot.fetch_guild(character.guild_id)
 
-    embed = Embed(
-      title=character.character_name,
-      url=str(character.sheet_link),
-    ).set_author(name=guild.name, icon_url=guild.icon.url if guild.icon else None)
+    embeds = [
+      Embed(
+        title=character.character_name,
+        url=str(character.sheet_link),
+      ).set_author(name=guild.name, icon_url=guild.icon.url if guild.icon is not None else None)
+    ]
+
+    # Add fields
+    embeds[0].add_field(
+      name=f"Character Level: {character.level}",
+      value="Character Classes:\n" + "\n".join((f"{classname}: {level}" for classname, level in character.classes.root.items())),
+      inline=True,
+    )
+    embeds[0].add_field(name=f"Milestones: {character.milestones}", value="", inline=True)
+    embeds[0].add_field(name=f"Mythic Tier: {character.tier}", value=f"Mythic Trials: {character.mythic_trials}", inline=True)
+    embeds[0].add_field(
+      name="Money:",
+      value=(
+        f"Platinum: {character.money_pp}\n"
+        f"    Gold: {character.money_gp}\n"
+        f"  Silver: {character.money_sp}\n"
+        f"  Copper: {character.money_cp}"  # keep expanded
+      ),
+      inline=True,
+    )
+
+    if character.token_url is not None:
+      embeds[0].set_thumbnail(url=str(character.token_url))
+
+    if character.images:
+      embeds[0].set_image(url=character.images.root[0])
+      embeds.extend(Embed().set_image(url=str(url)) for url in character.images.root[1:])
+
+    await inter.send(embeds=embeds)
+
 
 
 def setup(bot: "SwallowBot"):
