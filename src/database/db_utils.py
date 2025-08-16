00681a73cfcb1930f614304407d1a83c9d70f4f9
Jacob Ogden <jacob.ogden@yahoo.com>
2025-08-16 02:10:32 -0400

    Fixed an error where the Database would attempt to initialize before the asyncio event loop was started

--- src/database/db_utils.py
+++ src/database/db_utils.py
@@ -3,14 +3,14 @@ from validation.models.db_entries import GuildDBEntryModel, UserDBEntryModel
 
 from database.cache import DatabaseCache
 
-db = DatabaseCache()
-
 
 async def ensure_user_exists(user: User, guild: Guild) -> None:
-  if not await db.users.check_exist((user.id, guild.id)):
+  db = DatabaseCache()
+  if not await db.users.check_exists((user.id, guild.id)):
     await db.users.append_row(UserDBEntryModel(user_id=user.id, guild_id=guild.id, user_name=user.name))
 
 
 async def ensure_guild_exists(guild: Guild) -> None:
-  if not await db.guilds.check_exist(guild.id):
+  db = DatabaseCache()
+  if not await db.guilds.check_exists(guild.id):
     await db.guilds.append_row(GuildDBEntryModel(guild_id=guild.id, guild_name=guild.name))
