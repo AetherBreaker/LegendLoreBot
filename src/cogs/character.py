0b101c2e9e7091acdc188ad3750d6531783de26c
Jacob Ogden <jacob.ogden@yahoo.com>
2025-08-16 02:19:50 -0400

    Added some lightly tested commands for character art and tokens

--- src/cogs/character.py
+++ src/cogs/character.py
@@ -4,11 +4,12 @@ if __name__ == "__main__":
   configure_logging()
 
 from logging import getLogger
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
 from autocomplete.command_autocompleters import autocomp_charname
-from disnake import ApplicationCommandInteraction, Embed, GuildCommandInteraction, Member, User
-from disnake.ext.commands import Cog, Param, message_command, slash_command, user_command
+from disnake import ApplicationCommandInteraction, Embed
+from disnake.ext.commands import Cog, Param, slash_command
+from pydantic import ValidationError
 
 if TYPE_CHECKING:
   from bot_base import SwallowBot
@@ -77,6 +78,85 @@ class CharacterTracking(Cog):
 
     await inter.send(embeds=embeds)
 
+  # Art and images
+
+  @characters.sub_command()
+  async def add_art(
+    self,
+    inter: ApplicationCommandInteraction,
+    character_name: str,
+    image_url: str,
+  ):
+    character = (
+      await self.bot.database.characters.read_typed_row((inter.user.id, character_name))
+      if inter.guild_id is None
+      else await self.bot.database.characters.read_typed_row((inter.user.id, inter.guild_id, character_name))
+    )
+
+    if character is None:
+      await inter.send("Character not found.")
+      return
+
+    character.images.root.append(image_url)  # type: ignore
+    await self.bot.database.characters.update_row(character.character_uid, character)
+    await inter.send(f"Character art added.\n{image_url}")
+
+  @characters.sub_command()
+  async def remove_art(
+    self,
+    inter: ApplicationCommandInteraction,
+    character_name: str,
+    art_position: int = Param(description="Position of the art to remove, starting from 1."),
+  ):
+    character = (
+      await self.bot.database.characters.read_typed_row((inter.user.id, character_name))
+      if inter.guild_id is None
+      else await self.bot.database.characters.read_typed_row((inter.user.id, inter.guild_id, character_name))
+    )
+
+    if character is None:
+      await inter.send("Character not found.")
+      return
+
+    try:
+      removed_art = character.images.root.pop(art_position - 1)
+    except IndexError:
+      await inter.send("Art not found.")
+      return
+
+    await self.bot.database.characters.update_row(character.character_uid, character)
+    await inter.send(f"Removed art from character.\n{removed_art}")
+
+  @characters.sub_command()
+  async def set_token(
+    self,
+    inter: ApplicationCommandInteraction,
+    character_name: str,
+    token_url: str,
+  ):
+    character = (
+      await self.bot.database.characters.read_typed_row((inter.user.id, character_name))
+      if inter.guild_id is None
+      else await self.bot.database.characters.read_typed_row((inter.user.id, inter.guild_id, character_name))
+    )
+
+    if character is None:
+      await inter.send("Character not found.")
+      return
+
+    try:
+      character.token_url = token_url  # type: ignore
+      await self.bot.database.characters.update_row(character.character_uid, character)
+      await inter.send(f"Token URL set for character {character.character_name}: {token_url}.")
+
+    except ValidationError as e:
+      await inter.send("Invalid URL format.")
+      return
+
+  # Add the autocomplete function without having to make character_name the last param
+  add_art.autocomplete("character_name")(autocomp_charname)
+  remove_art.autocomplete("character_name")(autocomp_charname)
+  set_token.autocomplete("character_name")(autocomp_charname)
 
 
 def setup(bot: "SwallowBot"):
