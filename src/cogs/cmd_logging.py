6add53654348785fd0d1f062e309b7a5e198c197
Jacob Ogden <jacob.ogden@yahoo.com>
2025-08-16 23:42:44 -0400

    Preparations made to add command logging

---
+++ src/cogs/cmd_logging.py
@@ -0,0 +1,28 @@
+if __name__ == "__main__":
+  from logging_config import configure_logging
+
+  configure_logging()
+
+from logging import getLogger
+from typing import TYPE_CHECKING
+
+from autocomplete.command_autocompleters import autocomp_charname
+from disnake import ApplicationCommandInteraction, Embed
+from disnake.ext.commands import Cog, Param, slash_command
+from pydantic import ValidationError
+
+if TYPE_CHECKING:
+  from bot_base import SwallowBot
+
+
+logger = getLogger(__name__)
+
+
+class AppCMDLoggingCog(Cog):
+  def __init__(self, bot: "SwallowBot"):
+    self.bot = bot
+
+
+def setup(bot: "SwallowBot"):
+  bot.add_cog(AppCMDLoggingCog(bot))
+  print("AppCMDLoggingCog loaded.")
