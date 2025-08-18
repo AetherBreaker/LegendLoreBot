9942b558d88bba67168ef9cc59f1de210b3e6299
Jacob Ogden <jacob.ogden@yahoo.com>
2025-08-17 22:17:01 -0400

    Prepass_cache now accepts a max_size arguement for defining the size of the cache on a case by case basis

--- src/autocomplete/autocomp_cache.py
+++ src/autocomplete/autocomp_cache.py
@@ -2,16 +2,17 @@ from collections import UserDict
 from collections.abc import Callable, Hashable
 from functools import partial
 from sys import getsizeof
-from typing import Any, Concatenate
+from typing import Any, Concatenate, ParamSpec, TypeVar, overload
 
 
 class AutocompCache[Val_T](UserDict):
-  def __init__(self, *args: Any, **kwargs: Any) -> None:
+  def __init__(self, max_size: int = 5120, *args: Any, **kwargs: Any) -> None:
     super().__init__(*args, **kwargs)
     self.data: dict[Hashable, Val_T]
+    self.max_size = max_size
 
   def __setitem__(self, key: Hashable, item: Val_T) -> None:
-    if getsizeof(self) > 5120:
+    if getsizeof(self) > self.max_size:
       self.data.clear()
     super().__setitem__(key, item)
 
@@ -19,8 +20,32 @@ class AutocompCache[Val_T](UserDict):
     return super().get(key, default)
 
 
+SubP = ParamSpec("SubP")
+SubR = TypeVar("SubR")
+
+
+@overload
+def prepass_cache[SubR, **SubP](
+  func: None = None, /, *, max_size: int = 5120
+) -> Callable[[Callable[Concatenate[AutocompCache, SubP], SubR]], Callable[SubP, SubR]]: ...
+
+
+@overload
+def prepass_cache[SubR, **SubP](func: Callable[Concatenate[AutocompCache, SubP], SubR], /, *, max_size: int = 5120) -> Callable[SubP, SubR]: ...
+
+
 # create a decorator that creates an AutocompCache instance that will be shared across all instances of
 # the decorated function and prepass it into the decorated function using functools.partial
-def prepass_cache[R, **P](func: Callable[Concatenate[AutocompCache, P], R]) -> Callable[P, R]:
-  cache = AutocompCache()
+def prepass_cache[SubR, **SubP](
+  func=None, /, *, max_size=5120
+) -> Callable[[Callable[Concatenate[AutocompCache[Any], SubP], SubR]], Callable[SubP, SubR]] | Callable[SubP, SubR]:
+  if func is None:
+
+    def decorator(f: Callable[Concatenate[AutocompCache, SubP], SubR], /) -> Callable[SubP, SubR]:
+      cache = AutocompCache(max_size=max_size)
+      return partial(f, cache)
+
+    return decorator
+
+  cache = AutocompCache(max_size=max_size)
   return partial(func, cache)
