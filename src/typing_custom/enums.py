69b9ccef44f3a181f6038765a04190f71a8a2141
Jacob Ogden <jacob.ogden@yahoo.com>
2025-08-24 17:00:46 -0400

    Added and reordered list of custom event types

--- src/typing_custom/enums.py
+++ src/typing_custom/enums.py
@@ -1,8 +1,6 @@
 from enum import StrEnum, auto
 from typing import Any
 
-from disnake import Event
-
 
 class CustomStrEnum(StrEnum):
   """
@@ -19,6 +17,17 @@ class CustomStrEnum(StrEnum):
 
 class CustomEvent(CustomStrEnum):
   on_character_created = auto()
-  on_character_changed = auto()
   on_character_deleted = auto()
+
+  on_character_class_changed = auto()
+
+  on_character_milestones_changed = auto()
+  on_character_trials_changed = auto()
+  on_character_deeds_changed = auto()
+
+  on_character_money_changed = auto()
+  on_character_art_changed = auto()
+
+  on_attempted_change_own_data = auto()
+
   on_guild_setting_changed = auto()
